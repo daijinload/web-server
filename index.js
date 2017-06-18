3a01e077388f4066d911b902d3c6fe5df5836c8c
daijinload <daijinload@gmail.com>
2017-06-18 16:51:39 +0900

    Add New Files!!

---
+++ index.js
@@ -0,0 +1,10 @@
+
+const express = require('express');
+
+const port = 3000;
+const app = express();
+app.use(express.static(`${__dirname}/public`));
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
